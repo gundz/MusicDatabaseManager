e90d2c680a99acb947203c00ed0fab1262b16be2
gundz <fgundlac@student.42.fr>
2017-01-16 21:18:06 +0100

    removed useless functions

--- srcs/database_cron.php
+++ srcs/database_cron.php
@@ -11,50 +11,10 @@ function getMediaInfos( $file_path )
 	return ( $ret );
 }
 
-function calculateDirNodeAverageQuality( $folder_node )
-{
-	$files = $folder_node->getElementsByTagName( "file" );
-
-	$total_bitrate = 0;
-	$total_bitrate_modes = array();
-	$total_formats = array();
-
-	$nbrFiles = 0;
-	foreach ( $files as $file )
-	{
-		if ( $file->hasAttribute( "bitrate" ) )
-		{
-			$bitrate = $file->getAttribute( "bitrate" );
-			$total_bitrate += $bitrate;
-
-			$nbrFiles++;
-		}
-		if ( $file->hasAttribute( "bitrate_mode" ) )
-		{
-			$total_bitrate_modes[] = $file->getAttribute( "bitrate_mode" );
-		}
-		if ( $file->hasAttribute( "format" ) )
-		{
-			$total_formats[] = $file->getAttribute( "format" );
-		}
-	}
-	if ( $nbrFiles <= 0 )
-		return ( false );
-
-	$total_bitrate = floor( $total_bitrate / $nbrFiles );
-
-	$folder_node->setAttribute( 'folder_bitrate' , $total_bitrate );
-	$folder_node->setAttribute( 'folder_bitrate_format' , implode( ";" , array_unique( $total_formats ) ) );
-	$folder_node->setAttribute( 'folder_bitrate_mode' , implode( ";" , array_unique( $total_bitrate_modes ) ) );
-
-	return ( true );
-}
-
 function scanDirectory ( $dom , DirectoryIterator $dirIt, $dirNode = null)
 {
 	if ($dirNode == null)
 	{
-		echo "ok" .$dirIt->getPath() . PHP_EOL;
 		$dirNode = createDirNode( $dom , $dirIt->getPath() );
 	}
 	foreach ( $dirIt as $dir )
@@ -63,9 +23,7 @@ function scanDirectory ( $dom , DirectoryIterator $dirIt, $dirNode = null)
 		{
 			if (dirNodeExists( $dom , $dir->getPathName() ) == false)
 			{
-				echo "ok2" .  $dir->getPathname() . PHP_EOL;
 				$subDir = scanDirectory( $dom , new DirectoryIterator( $dir->getPathName() ) );
-				echo "ok3" . PHP_EOL;
 				$dirNode->appendChild( $subDir );
 			}
 		}
@@ -113,90 +71,11 @@ function createFileNode( $dom , $file_path )
 	return ( $node );
 }
 
-function updateFileNode( $dom , $file_path )
-{
-	$files = $dom->getElementsByTagName( "file" );
-
-	foreach ( $files as $file )
-	{
-		if ( $file->hasAttribute( "path" ) )
-		{
-			if ( $file->getAttribute( "path" ) == $file_path )
-			{
-				$new_node = createFileNode( $dom , $file_path );
-				$file->parentNode->replaceChild( $new_node , $file );
-				break ;
-			}
-		}
-	}
-	return ( $dom );
-}
-
-function deleteFileNode( $dom , $file_path )
-{
-	$files = $dom->getElementsByTagName( "file" );
-
-	foreach ( $files as $file )
-	{
-		if ( $file->hasAttribute( "path" ) )
-		{
-			if ( $file->getAttribute( "path" ) == $file_path )
-			{
-				$file->parentNode->removeChild( $file );
-				break ;
-			}
-		}
-	}
-	return ( $dom );
-}
-
 function createDirNode( $dom , $path )
 {
 	$dirNode = $dom->createElement( 'dir' );
 	$dirNode->setAttribute( 'path' , $path );
 	return ( $dirNode );
-}
-
-function updateDirNode( $dom , $dir_path )
-{
-	$dirs = $dom->getElementsByTagName( "dir" );
-
-	if ( !is_dir( $dir_path ) )
-		return ( deleteDirNode( $dom , $dir_path ) );
-
-	foreach ( $dirs as $dir )
-	{
-		if ( $dir->hasAttribute( "path" ) )
-		{
-			if ( $dir->getAttribute( "path" ) == $dir_path )
-			{
-				$new_node = scanDirectory( $dom , new DirectoryIterator( 
-					$dir_path ) );
-				$dir->parentNode->replaceChild( $new_node , $dir );
-				return ( $dom );
-			}
-		}
-	}
-
-	return ( $dom );
-}
-
-function deleteDirNode( $dom , $dir_path )
-{
-	$files = $dom->getElementsByTagName( "dir" );
-
-	foreach ( $files as $file )
-	{
-		if ( $file->hasAttribute( "path" ) )
-		{
-			if ( $file->getAttribute( "path" ) == $dir_path )
-			{
-				$file->parentNode->removeChild( $file );
-				break ;
-			}
-		}
-	}
-	return ( $dom );
 }
 
 function fileNodeExists( $dom , $file_path )
@@ -231,66 +110,6 @@ function dirNodeExists( $dom , $file_path )
 		}
 	}
 	return ( false );
-}
-
-function addFile( $dom , $file_path )
-{
-	$node = createFileNode( $dom , $file_path );
-	if ($node == null || fileNodeExists( $dom , $file_path ) )
-		return ( false );
-
-	$file_dir = dirname( $file_path );
-
-	$dirs = $dom->getElementsByTagName( "dir" );
-
-	foreach ( $dirs as $dir )
-	{
-		if ( $dir->hasAttribute( "path" ) )
-		{
-			if ( $dir->getAttribute( "path" ) == $file_dir )
-			{
-				$dir->appendChild( $node );
-				break ;
-			}
-		}
-	}
-	return ( true );
-}
-
-function removeFile( $dom , $file_path )
-{
-	$files = $dom->getElementsByTagName( "file" );
-
-	foreach ( $files as $file )
-	{
-		if ( $file->hasAttribute( "path" ) )
-		{
-			if ( $file->getAttribute( "path" ) == $file_path )
-			{
-				$file->parentNode->removeChild( $file );
-				break ;
-			}
-		}
-	}
-}
-
-function addDir( $dom , $dir_path )
-{
-	$node = scanDirectory( $dom , new DirectoryIterator( $dir_path ) );
-
-	$dirs = $dom->getElementsByTagName( "dir" );
-
-	foreach ( $dirs as $dir )
-	{
-		if ( $dir->hasAttribute( "path" ) )
-		{
-			if ( $dir->getAttribute( "path" ) == dirname($dir_path) )
-			{
-				$dir->appendChild( $node );
-				break ;
-			}
-		}
-	}
 }
 
 function checkXMLDirs( $dom )
@@ -345,28 +164,26 @@ function checkXMLFiles( $dom )
 	}
 }
 
-function addDir2( $dom , $dir_path , $root_path )
+function addDir( $dom , $dir_path , $root_path )
 {
 	$dirs = $dom->getElementsByTagName( "dir" );
 
-	echo "root_path = " . $root_path . PHP_EOL;
 	foreach ( $dirs as $dir )
 	{
 		if ( $dir->hasAttribute( "path" ) )
 		{
 			if ( $dir->getAttribute( "path" ) == $dir_path )
 			{
-				echo "found ! " . $dir->getAttribute( "path" ) . PHP_EOL;
 				scanDirectory( $dom , new DirectoryIterator( $dir->getAttribute( "path" ) ) , $dir );
 				return ;
 			}
 		}
 	}
 	if (dirname($dir_path) != $root_path)
-		addDir2($dom, dirname($dir_path), $root_path);
+		addDir($dom, dirname($dir_path), $root_path);
 }
 
-function addFile2( $dom , $file_path )
+function addFile( $dom , $file_path )
 {
 	$dirs = $dom->getElementsByTagName( "dir" );
 
@@ -376,7 +193,6 @@ function addFile2( $dom , $file_path )
 		{
 			if ( $dir->getAttribute( "path" ) == dirname($file_path) )
 			{
-				echo "found ! " . $dir->getAttribute( "path" ) . PHP_EOL;
 				$fileNode = createFileNode( $dom , $file_path );
 				if ($fileNode === null)
 					continue ;
@@ -405,7 +221,7 @@ function checkDirs( $dom , DirectoryIterator $dirIt , $dirRoot = null )
 			else
 			{
 				echo "DIR: \"" . $dir->getPathName() . "\" NOT EXISTS IN DB" . PHP_EOL;
-				addDir2( $dom , $dir->getPathName() , $dirRoot );
+				addDir( $dom , $dir->getPathName() , $dirRoot );
 			}
 			checkDirs( $dom , new DirectoryIterator( $dir->getPathName() ) , $dirRoot);
 		}
@@ -418,7 +234,7 @@ function checkDirs( $dom , DirectoryIterator $dirIt , $dirRoot = null )
 			else
 			{
 				echo "FILE: \"" . $dir->getPathName() . "\" NOT EXISTS IN DB" . PHP_EOL;
-				addFile2($dom, $dir->getPathname());
+				addFile($dom, $dir->getPathname());
 			}
 		}
 	}
@@ -437,12 +253,9 @@ if ( !file_exists( $xml_file_path ) )
 	$dom->save( $xml_file_path );
 }
 
-if ($argc == 1)
-{
-	$dom = new DOMDocument();
-	$dom->load( $xml_file_path );
-	updateDatabase( $dom , new DirectoryIterator($path) );
-	$dom->save( $xml_file_path );
-}
+$dom = new DOMDocument();
+$dom->load( $xml_file_path );
+updateDatabase( $dom , new DirectoryIterator($path) );
+$dom->save( $xml_file_path );
 
 ?>
