3c6d35a1e5b718b12cb23519ebf7c34bb6626399
gundz <fgundlac@student.42.fr>
2017-01-27 15:13:57 +0100

    adding country workd

--- database_srcs/post.php
+++ database_srcs/post.php
@@ -11,13 +11,27 @@ foreach ( $dirs as $dir )
 {
 	if ( $dir->hasAttribute( "path" ) )
 	{
-		if ( $_POST["pk"] == $dir->getAttribute( "path" ) )
+		if ($_POST["name"] == "comment")
 		{
-			$dir->setAttribute( 'comment' , $_POST["value"] );
-			$dom->save( $xml_file_path );
-			break ;
+			if ( $_POST["pk"] == $dir->getAttribute( "path" ) )
+			{
+				$dir->setAttribute( 'comment' , $_POST["value"] );
+				$dom->save( $xml_file_path );
+				exit(http_response_code(200));
+			}
+		}
+		if ($_POST["name"] == "country")
+		{
+			if ( $_POST["pk"] == $dir->getAttribute( "path" ) )
+			{
+				$dir->setAttribute( 'country' , $_POST["value"] );
+				$dom->save( $xml_file_path );
+				exit(http_response_code(200));
+			}
 		}
 	}
 }
+
+exit(http_response_code(500));
 
 ?>
